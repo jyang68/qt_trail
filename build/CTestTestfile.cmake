8b59b7dc2b3c84bb97400d2278f9508dc868d6e9
Jian <jyang68>
2019-09-16 00:47:53 +0800

    qt_trail

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jian/qt_trail/src
+# Build directory: /home/jian/qt_trail/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gmock)
+subdirs(qtros)
---
+++ build/qtros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jian/qt_trail/src/qtros
+# Build directory: /home/jian/qt_trail/build/qtros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
